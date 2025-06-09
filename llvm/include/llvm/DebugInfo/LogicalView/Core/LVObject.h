2b0819cbc87e1ccd82254dfdbb431c20739097c2
Javier Lopez-Gomez <javier.lopez.gomez@proton.me>
2025-06-09 17:12:31 +0200

    [llvm-debuginfo-analyzer][debuginfologicalview] Fix ODR violation in LVObject
    Fix ODR violation in `LVObject` when `DebugInfoLogicalView` library
    is used in a downstream project.
    
    Depending solely on `NDEBUG` for exposing some data members may cause
    trouble in downstream projects.
    Enable such cases only if `LLVM_ENABLE_ABI_BREAKING_CHECKS`.

--- llvm/include/llvm/DebugInfo/LogicalView/Core/LVObject.h
+++ llvm/include/llvm/DebugInfo/LogicalView/Core/LVObject.h
@@ -15,6 +15,7 @@
 #define LLVM_DEBUGINFO_LOGICALVIEW_CORE_LVOBJECT_H
 
 #include "llvm/BinaryFormat/Dwarf.h"
+#include "llvm/Config/abi-breaking.h"
 #include "llvm/DebugInfo/CodeView/CodeView.h"
 #include "llvm/DebugInfo/CodeView/TypeIndex.h"
 #include "llvm/DebugInfo/LogicalView/Core/LVSupport.h"
@@ -154,7 +155,7 @@ class LVObject {
   // copy constructor to create that object; it is used to print a reference
   // to another object and in the case of templates, to print its encoded args.
   LVObject(const LVObject &Object) {
-#ifndef NDEBUG
+#if !defined(NDEBUG) && LLVM_ENABLE_ABI_BREAKING_CHECKS
     incID();
 #endif
     Properties = Object.Properties;
@@ -165,7 +166,7 @@ class LVObject {
     Parent = Object.Parent;
   }
 
-#ifndef NDEBUG
+#if !defined(NDEBUG) && LLVM_ENABLE_ABI_BREAKING_CHECKS
   // This is an internal ID used for debugging logical elements. It is used
   // for cases where an unique offset within the binary input file is not
   // available.
@@ -193,7 +194,7 @@ protected:
 
 public:
   LVObject() {
-#ifndef NDEBUG
+#if !defined(NDEBUG) && LLVM_ENABLE_ABI_BREAKING_CHECKS
     incID();
 #endif
   };
@@ -317,7 +318,7 @@ public:
 
   uint64_t getID() const {
     return
-#ifndef NDEBUG
+#if !defined(NDEBUG) && LLVM_ENABLE_ABI_BREAKING_CHECKS
         ID;
 #else
         0;
